119e2db899a2f4141f2d038eef9fde37eb08e07e
Valentyn Radchenko <valentyn.radchenko@outlook.com>
2025-04-02 15:38:24 +0200

    Lab 02 comleted

---
+++ Model/Person.cs
@@ -0,0 +1,84 @@
+﻿using Lab.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.Model
+{
+	public class Person
+	{
+		public string Name { get; set; }
+		public string SecondName { get; set; }
+		public string Email { get; set; }
+		public DateTime DateOfBirth { get; set; }
+
+		private readonly bool _isAdult;
+		private readonly string _sunSign;
+		private readonly string _chineseSign;
+		private readonly bool _isBirthday;
+
+		public bool IsAdult => _isAdult;
+		public string SunSign => _sunSign;
+		public string ChineseSign => _chineseSign;
+		public bool IsBirthday => _isBirthday;
+
+		public Person(string name, string secondName, string email, DateTime dateOfBirth)
+		{
+			Name = name;
+			SecondName = secondName;
+			Email = email;
+			DateOfBirth = dateOfBirth;
+
+			_isAdult = CalculateIsAdult();
+			_sunSign = CalculateSunSign();
+			_chineseSign = CalculateChineseSign();
+			_isBirthday = CalculateIsBirthday();
+		}
+		public Person(string name, string secondName, DateTime dateOfBirth)
+		{
+			Name = name;
+			SecondName = secondName;
+			Email = "";
+			DateOfBirth = dateOfBirth;
+		}
+		public Person(string name, string secondName, string email)
+		{
+			Name = name;
+			SecondName = secondName;
+			Email = email;
+			DateOfBirth = DateTime.Now;
+		}
+		private bool CalculateIsAdult()
+		{
+			if (DateOfBirth == default) return false;
+			var today = DateTime.Today;
+			int age = today.Year - DateOfBirth.Year;
+			if (DateOfBirth > today.AddYears(-age)) age--;
+			return age >= 18;
+		}
+
+		private string CalculateSunSign()
+		{
+			if (DateOfBirth == default) return "Unknown: Date of birth is not given";
+
+			return ZodiacHelper.GetWesternZodiac(DateOfBirth);
+		}
+
+		private string CalculateChineseSign()
+		{
+			if (DateOfBirth == default) return "Unknown: Date of birth is not given";
+
+			return ZodiacHelper.GetChineseZodiac(DateOfBirth);
+		}
+
+		private bool CalculateIsBirthday()
+		{
+			if (DateOfBirth == default) return false;
+			var today = DateTime.Today;
+			return today.Month == DateOfBirth.Month && today.Day == DateOfBirth.Day;
+		}
+	}
+}
---
+++ ViewModel/PersonViewModel.cs
@@ -0,0 +1,136 @@
+﻿using Lab.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using Lab.Model;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Lab.Utility;
+
+namespace Lab.ViewModel
+{
+	public class PersonViewModel: INotifyPropertyChanged 
+	{
+		public string Name
+		{
+			get => _name;
+			set
+			{
+				if (_name != value)
+				{
+					_name = value;
+					OnPropertyChanged();
+				}
+			}
+		}
+		public string SecondName
+		{
+			get => _secondName;
+			set
+			{
+				if (_secondName != value)
+				{
+					_secondName = value;
+					OnPropertyChanged();
+				}
+			}
+		}
+		public string Email
+		{
+			get => _email;
+			set
+			{
+				if (_email != value)
+				{
+					_email = value;
+					OnPropertyChanged();
+				}
+			}
+		}
+		public DateTime DateOfBirth
+		{
+			get => _dateOfBirth;
+			set
+			{
+				if (_dateOfBirth != value)
+				{
+					_dateOfBirth = value;
+					OnPropertyChanged();
+				}
+			}
+		} 
+
+		private string _name;
+		private string _secondName;
+		private string _email;
+		private DateTime _dateOfBirth = DateTime.Today;
+
+
+
+		private RelayCommand _proceedCommand;
+		public ICommand ProceedCommand => _proceedCommand;
+
+		public PersonViewModel()
+		{
+			_proceedCommand = new RelayCommand(_ => ShowPersonInfo(), _ => CanProceed());
+		}
+
+
+		public Person Person { get; set; }
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+			_proceedCommand.RaiseCanExecuteChanged();
+		}
+
+		private bool CanProceed()
+		{
+			return !string.IsNullOrWhiteSpace(Name) &&
+			!string.IsNullOrWhiteSpace(SecondName) &&
+			!string.IsNullOrWhiteSpace(Email) &&
+			DateOfBirth != default;
+		}
+
+		private void ShowPersonInfo()
+		{
+			int age = DateTime.Today.Year - DateOfBirth.Year;
+			if (DateOfBirth > DateTime.Today.AddYears(-age)) age--;
+
+			if (DateOfBirth > DateTime.Today || age > 135)
+			{
+				MessageBox.Show("Невірна дата народження. Людина ще не народилась або їй більше ніж 135 років.");
+				return;
+			}
+
+			// Перевірка на день народження
+			if (DateOfBirth.Day == DateTime.Today.Day &&
+				DateOfBirth.Month == DateTime.Today.Month)
+			{
+				MessageBox.Show("З Днем Народження! 🎉");
+			}
+
+			// Створити об’єкт Person і показати всі 8 полів
+			var person = new Person(Name, SecondName, Email, DateOfBirth);
+
+			string info = $"Name: {person.Name}\n" +
+						  $"Second Name: {person.SecondName}\n" +
+						  $"Email: {person.Email}\n" +
+						  $"Date of Birth: {person.DateOfBirth.ToShortDateString()}\n" +
+						  $"IsAdult: {person.IsAdult}\n" +
+						  $"SunSign: {person.SunSign}\n" +
+						  $"ChineseSign: {person.ChineseSign}\n" +
+						  $"IsBirthday: {person.IsBirthday}";
+
+			MessageBox.Show(info, "Person Info");
+		}
+	}
+}
+
